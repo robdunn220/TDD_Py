a44e0bf35625bb28cb9cc9f74e7ad3a3b45fa1a1
robdunn220 <robertdunn220@gmail.com>
2019-01-31 15:54:42 -0500

    Adding funstionality to add and test for multiple items to to-do list

--- Ch_1/functional_tests.py
+++ Ch_1/functional_tests.py
@@ -11,6 +11,11 @@ class NewVisitorTest(unittest.TestCase):
     def tearDown(self):
         self.browser.quit()
 
+    def check_for_row_in_list_table(self, row_text):
+        table = self.browser.find_element_by_id('id_list_table')
+        rows = table.find_elements_by_tag_name('tr')
+        self.assertIn(row_text, [row.text for row in rows])
+
     def test_start_and_retrieve_list(self):
         self.browser.get('http://localhost:8000')
         self.assertIn('To-Do', self.browser.title)
@@ -26,13 +31,21 @@ class NewVisitorTest(unittest.TestCase):
         inputbox.send_keys('Buy feathers')
         inputbox.send_keys(Keys.ENTER)
         time.sleep(1)
+        self.check_for_row_in_list_table('1: Buy feathers')
+
+        inputbox = self.browser.find_element_by_id('id_new_item')
+        inputbox.send_keys('Use feathers to make a fly')
+        inputbox.send_keys(Keys.ENTER)
+        time.sleep(1)
+
+        self.check_for_row_in_list_table('1: Buy feathers')
+        self.check_for_row_in_list_table('2: Use feathers to make a fly')
 
         table = self.browser.find_element_by_id('id_list_table')
-        rows = table.find_element_by_tag_name('tr')
-        self.assertTrue(
-            any(row.text == '1: Buy feathers' for row in rows)
-        )
-        
+        rows = table.find_elements_by_tag_name('tr')
+        self.assertIn('1: Buy feathers', [row.text for row in rows])
+        self.assertIn('2: Use feathers to make a fly', [row.text for row in rows])
+
         self.fail('Finish the test')
 
 if __name__ == '__main__':
--- Ch_1/lists/views.py
+++ Ch_1/lists/views.py
@@ -1,6 +1,9 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.views.decorators.csrf import ensure_csrf_cookie
 
 # Create your views here.
 def home_page(request):
-    return render(request, 'home.html')
+    return render(request, 'home.html', {
+        'new_item_text': request.POST.get('item_text', ''),
+    })
